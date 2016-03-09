3a871078c3f4a9b5f11213fd9d9f7ba9a50f4f33
Li Xiangqun <lixiangqun@yaodeMacBook-Air.local>
2016-03-09 23:19:32 +0800

    removed visited

--- Second/Main.java
+++ Second/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	private boolean useTable;
 	private int numCity;
 	private int numQuery;
-	private int[] distance;
+	private int[] disTableOrFestiveTable;
 	private int[][] queries;
 	private ArrayList<ArrayList<Integer>> adjList;
 
@@ -19,27 +19,32 @@ public class Main {
 		Scanner myScanner = new Scanner(System.in);
 		numCity = myScanner.nextInt();
 		numQuery = myScanner.nextInt();
-		distance = new int[numCity+1];
-		Arrays.fill(distance, -1);
+		
+		disTableOrFestiveTable = new int[numCity+1];
+		Arrays.fill(disTableOrFestiveTable, -1);
 		adjList = new ArrayList<ArrayList<Integer>>();
 		for (int i = 0; i < numCity+1; ++i) {
 			adjList.add(new ArrayList<Integer>());
 		}
+		
 		for (int i = 0; i < numCity-1; ++i) {
 			int first = myScanner.nextInt();
 			int second = myScanner.nextInt();
 			adjList.get(first).add(second);
 			adjList.get(second).add(first);
 		}
+		
 		queries = new int[numQuery][2];
 		int updateNum = 0, queryNum = 0; 
 		for (int i = 0; i < numQuery; ++i) {
 			queries[i][0] = myScanner.nextInt();
+			queries[i][1] = myScanner.nextInt();
+			
 			if (queries[i][0] == 1) updateNum++;
 			else	queryNum++;
-			queries[i][1] = myScanner.nextInt();
 		}
 		myScanner.close();
+		
 		//decide mode
 		this.useTable = queryNum >= updateNum;
 		
@@ -53,7 +58,7 @@ public class Main {
 	}
 	private void getDistance(int city) {
 		if (useTable)
-			System.out.println(distance[city]);
+			System.out.println(disTableOrFestiveTable[city]);
 		else{
 
 			System.out.println(bfsGetDistance(city));
@@ -62,19 +67,17 @@ public class Main {
 	private int bfsGetDistance(int city) {
 		int dist = 0;
 		
-		boolean[] visited = new boolean[this.numCity+1];
 		Queue<int[]> myQ = new LinkedList<int[]>();
-		myQ.add(new int[]{city, 0});
+		myQ.add(new int[]{city, 0, -1});
 		while(!myQ.isEmpty()) {
 			int[] cd = myQ.poll();
-			if (distance[cd[0]] == 1){
-				dist = cd[1];
+			if (disTableOrFestiveTable[cd[0]] == 1){
+				dist = cd[1];//found result
 				break;
 			}
 			else {
-				visited[cd[0]] = true;
 				for (int i : adjList.get(cd[0])) {
-					if (visited[i]) continue;
+					if (i == cd[2]) continue;//avoid traverse back to parent
 					myQ.add(new int[]{i, cd[1]+1});
 				}
 			}
@@ -84,39 +87,29 @@ public class Main {
 
 	private void updateDistance(int city) {
 		if (useTable)
-			bfs(0, city);
+			bfsUpdateDistanceTable(0, city);
 		else
-			distance[city] = 1;
+			disTableOrFestiveTable[city] = 1;
 	}
 
 	private void intializeDistance() {
 		if (useTable)
-			bfs(0, 1);
+			bfsUpdateDistanceTable(0, 1);
 		else
-			distance[0] = 1;
+			disTableOrFestiveTable[0] = 1;
 	}
-//	
-//	private void dfs(int dist, int city) {
-//		if (distance[city] != -1 && distance[city] <= dist) return;
-//		distance[city] = dist;
-//		for (int i : adjList.get(city)) {
-//			dfs(dist+1, i);
-//		}
-//	}
-	private void bfs(int dist, int city) {
-		boolean[] visited = new boolean[this.numCity+1];
-		if (distance[city] != -1 && distance[city] <= dist) return;
+	
+	private void bfsUpdateDistanceTable(int dist, int city) {
 		Queue<int[]> myQ = new LinkedList<int[]>();
-		myQ.add(new int[]{city, dist});
+		myQ.add(new int[]{city, dist, -1});
 		while(!myQ.isEmpty()) {
 			int[] cd = myQ.poll();
-			if (distance[cd[0]] == -1 || distance[cd[0]] > cd[1])
+			if (disTableOrFestiveTable[cd[0]] == -1 || disTableOrFestiveTable[cd[0]] > cd[1])
 			{
-				distance[cd[0]] = cd[1];
-				visited[cd[0]] = true;
+				disTableOrFestiveTable[cd[0]] = cd[1];
 				for (int i : adjList.get(cd[0])) {
-					if (visited[i]) continue;
-					myQ.add(new int[]{i, cd[1]+1});
+					if (i == cd[2]) continue;//avoid traverse back to parent
+					myQ.add(new int[]{i, cd[1]+1, cd[0]});
 				}
 			}
 		}
